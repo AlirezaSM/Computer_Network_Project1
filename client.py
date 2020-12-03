1b001b18ece5e915c4434bc519056cc18ac5da63
AlirezaSM <sedighi_a77@yahoo.com>
2020-12-03 14:44:21 +0330

    Showing users list to client and choosing a client added.

--- client.py
+++ client.py
@@ -1,24 +1,82 @@
 import socket
 
 PORT = 7447
-
 MESSAGE_LENGTH_SIZE = 64
-
 ENCODING = 'utf-8'
 
 
 def main():
 
     address = socket.gethostbyname(socket.gethostname())
-
     SERVER_INFORMATION = (address, PORT)
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     s.connect(SERVER_INFORMATION)
 
-    send_msg(s, "HELLO WORLD!!")
-    send_msg(s, "DISCONNECT")
+    username = set_username(s)
+    print("""- '/users' showing accessible users
+- '/choose_user' choose a user for messaging
+- '/disconnect' disconnect from server""")
+
+    while True:
+        order = input('Enter your order: ')
+        send_msg(s, order)
+
+        if order == '/disconnect':
+            break
+        elif order == '/users':
+            show_accessible_users(s)
+        elif order == '/choose_user':
+            choose_target_user(s)
+
+    # send_msg(s, "HELLO WORLD!!")
+    # send_msg(s, "DISCONNECT")
+
+
+def choose_target_user(client):
+    target_user = 'not set'
+    response = 'not set'
+    while response != 'connected':
+        target_user = input('Enter target username: ')
+        send_msg(client, target_user)
+        response = get_msg(client)
+
+        if response == 'connected':
+            print("Connected to {}".format(target_user))
+        elif response == 'user not found':
+            print("User not found")
+
+    if response == 'connected':
+        message_to_user(client, target_user)
+
+
+def message_to_user(client, target_user):
+    print(get_msg(client))
+
+
+def show_accessible_users(client):
+    users = get_msg(client)
+    print("Accessible users: ", users)
+
+
+def set_username(client):
+    username = 'not set'
+    response = 'not set'
+    while response != 'free':
+        username = input('Enter your Username: ')
+        send_msg(client, username)
+        response = get_msg(client)
+
+        if response == 'free':
+            print("Username set")
+        elif response == 'reserved':
+            print("Username reserved")
+
+    return username
+
+def get_msg(client):
+    message_length = int(client.recv(MESSAGE_LENGTH_SIZE).decode(ENCODING))
+    msg = client.recv(message_length).decode(ENCODING)
+    return msg
 
 def send_msg(client, msg):
 
--- server.py
+++ server.py
@@ -2,18 +2,16 @@ import socket
 import threading
 
 PORT = 7447
-
 MESSAGE_LENGTH_SIZE = 64
-
 ENCODING = 'utf-8'
+user_list = []
+
 
 def main():
+
     address = socket.gethostbyname(socket.gethostname())
-
     HOST_INFORMATION = (address, PORT)
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     s.bind(HOST_INFORMATION)
 
     print("[SERVER STARTS] Server is starting ...")
@@ -26,11 +24,8 @@ def start(server):
     server.listen()
 
     while True:
-
         conn, address = server.accept()
-
         t = threading.Thread(target = handle_client, args = (conn, address))
-
         t.start()
 
 
@@ -38,20 +33,88 @@ def handle_client(conn, address):
 
     print("[NEW CONNECTION] connected from {}".format(address))
 
-    Connected = True
+    username = set_username(conn, address)
 
-    while Connected:
+    connected = True
+    while connected:
+        order = get_msg(conn)
+        print("[MESSAGE RECEIVED from {}] {}".format(username, order))
 
-        message_length = int(conn.recv(MESSAGE_LENGTH_SIZE).decode(ENCODING))
-
-        msg = conn.recv(message_length).decode(ENCODING)
-
-        print("[MESSAGE RECEIVED] {}".format(msg))
-
-        if msg == "DISCONNECT":
-            Connected = False
+        if order == '/disconnect':
+            print("[CONNECTION CLOSED] connection closed from {}".format(username))
+            user_list.remove([username, address[0], address[1]])
+            connected = False
+        elif order == '/users':
+            send_users_list(conn, username)
+        elif order == '/choose_user':
+            set_target_user(conn)
 
     conn.close()
+
+
+def set_target_user(client):
+    is_target_user_set = False
+    target_user = 'not set'
+    while not is_target_user_set:
+        target_user = get_msg(client)
+        if not is_username_free(target_user):
+            send_msg(client, "connected")
+            is_username_set = True
+            message_to_user(client, target_user)
+        else:
+            send_msg(client, "user not found")
+
+
+def message_to_user(client, target_user):
+    send_msg(client, "you can send message now")
+
+
+def send_users_list(client, username):
+    username_list = list(map(lambda x: x[0], user_list))
+    username_list.remove(username)
+    send_msg(client, str(username_list))
+
+
+def set_username(client, address):
+    is_username_set = False
+    username = 'not set'
+    while not is_username_set:
+        username = get_msg(client)
+        if is_username_free(username):
+            send_msg(client, "free")
+            user_list.append([username, address[0], address[1]])
+            is_username_set = True
+        else:
+            send_msg(client, "reserved")
+
+    return username
+
+def is_username_free(username):
+    is_free = True
+    for user in user_list:
+        if username == user[0]:
+            is_free = False
+
+    return is_free
+
+
+def get_msg(client):
+    message_length = int(client.recv(MESSAGE_LENGTH_SIZE).decode(ENCODING))
+    msg = client.recv(message_length).decode(ENCODING)
+    return msg
+
+
+def send_msg(client, msg):
+
+    message = msg.encode(ENCODING)
+
+    msg_length = len(message)
+    msg_length = str(msg_length).encode(ENCODING)
+    msg_length += b' ' * (MESSAGE_LENGTH_SIZE - len(msg_length))
+
+    client.send(msg_length)
+    client.send(message)
+
 
 if __name__ == '__main__':
     main()
